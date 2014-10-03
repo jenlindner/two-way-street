7cacd429dd665e0bdc4267c80aec7cb09f78a0ea
Jennifer Lindner <jenlindner@gmail.com>
2014-10-03 13:40:10 -0500

    dict object for template

--- engage.py
+++ engage.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import requests
 import csv
+import StringIO
 
 app = Flask(__name__)
 
@@ -8,10 +9,15 @@ app = Flask(__name__)
 @app.route('/engage')
 def engage():
     data_url = 'https://raw.githubusercontent.com/jenlindner/two-way-street/master/engagement.csv'
-    data = requests.get(data_url)
-    print data.content
 
-    return render_template('engage.html', data=data.content)
+    response = requests.get(data_url)
+    data = StringIO.StringIO(response.text)
+    data_dict = []
+
+    for row in csv.DictReader(data):
+      data_dict.append(row)
+
+    return render_template('engage.html', data=data_dict)
 
 
 if __name__ == '__main__':
