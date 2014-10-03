09cfaf24543a17dd7ec4a9c77b81d0121f8fb582
Jennifer Lindner <jenlindner@gmail.com>
2014-10-03 13:06:48 -0500

    getting github csv data into template

--- engage.py
+++ engage.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+import requests
+import csv
+
 app = Flask(__name__)
 
 
-@app.route('/engage', methods=['GET', 'POST'])
+@app.route('/engage')
 def engage():
-    if request.method == 'POST':
-        #update_engagement()
-        return render_template('engage.html', {})
-    else:
-        #show_engage()
-        return render_template('engage.html')
+    data_url = 'https://raw.githubusercontent.com/jenlindner/two-way-street/master/engagement.csv'
+    data = requests.get(data_url)
+    print data.content
+
+    return render_template('engage.html', data=data.content)
 
 
 if __name__ == '__main__':
